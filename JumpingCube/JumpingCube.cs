00d56d0e6c4d55de593c36d99214050e68860510
ArPaVa <arianpzv@gmail.com>
2024-11-10 12:21:19 -0500

    Score,Highscore, replay mechanic, growing difficulty

--- JumpingCube/JumpingCube.cs
+++ JumpingCube/JumpingCube.cs
@@ -11,7 +11,12 @@ using Microsoft.Xna.Framework.Media;
 namespace JumpingCube;
 
 public class JumpingCube : Game
-{   
+{   private bool died;
+    private bool double_jump;
+    private int double_jump_timer;
+    private int score;
+    private int highscore;
+    private int next_difficulty;
     private Texture2D cube;
     private int[] Cube_position {get; set;}
     private int[] Cube_dimensions {get; set;}
@@ -26,8 +31,8 @@ public class JumpingCube : Game
 
     private int x_impulse { get; set; }
     private GraphicsDeviceManager _graphics;
-    private SpriteBatch _spriteBatch;
-
+    private SpriteBatch _spriteBatch;   
+    private SpriteFont scores_font;
     private int next_obstacle {get; set;}
     public JumpingCube()
     {
@@ -40,7 +45,15 @@ public class JumpingCube : Game
     }
 
     protected override void Initialize()
-    {
+    {   
+        highscore = 0;
+        score = 0;
+        next_difficulty = 0;
+        
+        died = false;
+        double_jump = true;
+        double_jump_timer = 0;
+
         Cube_position =  new int[2];
         Cube_position[0]= 30;
         Cube_position[1]= _graphics.PreferredBackBufferHeight-_graphics.PreferredBackBufferHeight/4 - _graphics.PreferredBackBufferHeight/10;
@@ -67,6 +80,7 @@ public class JumpingCube : Game
         jumping_sound = Content.Load<SoundEffect>("jumping_sound");
         pixel = new Texture2D(GraphicsDevice, 1, 1);
         rectangle = Content.Load<Texture2D>("rectangle");
+        scores_font = Content.Load<SpriteFont>("scores_font");
         Color[] data = new Color[1];
         data[0] = Color.White; 
         pixel.SetData(data);
@@ -74,60 +88,97 @@ public class JumpingCube : Game
     }
 
     protected override void Update(GameTime gameTime)
-    {
-        for (int i = 0; i<Bg_rect.Count;i++)
-        {   Rectangle cube_rectangle  = new Rectangle (Cube_position[0],Cube_position[1],Cube_dimensions[0],Cube_dimensions[1]); 
-            Rectangle rectangle_rectangle = new Rectangle (Bg_rect[i].x_pos,Bg_rect[i].y_pos-Rectangles_dimensions[1],Rectangles_dimensions[0],Rectangles_dimensions[1]);
-            if (cube_rectangle.Intersects(rectangle_rectangle))
-                Exit();
-
-        }
-        
-        if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
-            Exit();
-
-        if (Cube_position[1] + x_impulse > _graphics.PreferredBackBufferHeight-_graphics.PreferredBackBufferHeight/4 - _graphics.PreferredBackBufferHeight/10)
-            Cube_position[1] = _graphics.PreferredBackBufferHeight-_graphics.PreferredBackBufferHeight/4 - _graphics.PreferredBackBufferHeight/10;
-        else
-            Cube_position[1] += x_impulse;
-
-        if (Cube_position[1] >=  _graphics.PreferredBackBufferHeight-_graphics.PreferredBackBufferHeight/4 )
-            Cube_position[1] = _graphics.PreferredBackBufferHeight-_graphics.PreferredBackBufferHeight/4 - _graphics.PreferredBackBufferHeight/10;
-        if ((-(Cube_dimensions[0]/10) < x_impulse && x_impulse<0) ||  (x_impulse< Cube_dimensions[0]/10 && x_impulse> 0))
-            x_impulse += Cube_dimensions[0]/60;
-        else
-            if (x_impulse <= Cube_dimensions[0]/4)
-                x_impulse +=Cube_dimensions[0]/40;
-            
-        if (CubeIsSuported())
-            x_impulse = 0;
-
-        if (CubeIsSuported() && Keyboard.GetState().IsKeyDown(Keys.Space))
-        {   x_impulse = -(Cube_dimensions[0]/3);
-            jumping_sound.Play();
-        }
-        if (Bg_rect != null)
-        for (int i= 0;i < Bg_rect.Count;i ++)
+    {   if(!died)
         {
-            Bg_rect[i].x_pos -= speed;
-            if (Bg_rect[i].x_pos<-70)
-                Bg_rect.RemoveAt(i);
+            next_difficulty +=1;
+            if (next_difficulty>=250)
+            {next_difficulty = 0;
+            speed += Cube_dimensions[0]/70;
 
-        }
-        next_obstacle-= speed;
-        if (next_obstacle <= 0)
-        {
-            Random random = new Random();
-            int randm = random.Next(1,30);
-            if (randm >= 30-speed)
-            {
-                Bg_rect.Add(new BadGuyRectangle(_graphics.PreferredBackBufferWidth,_graphics.PreferredBackBufferHeight-_graphics.PreferredBackBufferHeight/4,rectangle));
-                int randm_gap = random.Next(1,Rectangles_dimensions[1]);
-                next_obstacle = 3*Rectangles_dimensions[1] + randm_gap;
+            }
+            score += 1;
+            for (int i = 0; i<Bg_rect.Count;i++)
+            {   Rectangle cube_rectangle  = new Rectangle (Cube_position[0],Cube_position[1],Cube_dimensions[0],Cube_dimensions[1]); 
+                Rectangle rectangle_rectangle = new Rectangle (Bg_rect[i].x_pos,Bg_rect[i].y_pos-Rectangles_dimensions[1],Rectangles_dimensions[0],Rectangles_dimensions[1]);
+                if (cube_rectangle.Intersects(rectangle_rectangle))
+                    died = true;
                 
             }
+            if (!died)
+            {
+                if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                    Exit();
 
-        }
+                if (Cube_position[1] + x_impulse > _graphics.PreferredBackBufferHeight-_graphics.PreferredBackBufferHeight/4 - _graphics.PreferredBackBufferHeight/10)
+                    Cube_position[1] = _graphics.PreferredBackBufferHeight-_graphics.PreferredBackBufferHeight/4 - _graphics.PreferredBackBufferHeight/10;
+                else
+                    Cube_position[1] += x_impulse;
+
+                if (Cube_position[1] >=  _graphics.PreferredBackBufferHeight-_graphics.PreferredBackBufferHeight/4 )
+                    Cube_position[1] = _graphics.PreferredBackBufferHeight-_graphics.PreferredBackBufferHeight/4 - _graphics.PreferredBackBufferHeight/10;
+                if ((-(Cube_dimensions[0]/10) < x_impulse && x_impulse<0) ||  (x_impulse< Cube_dimensions[0]/10 && x_impulse> 0))
+                    x_impulse += Cube_dimensions[0]/60;
+                else
+                    if (x_impulse <= Cube_dimensions[0]/4)
+                        x_impulse +=Cube_dimensions[0]/40;
+                    
+                if (CubeIsSuported())
+                    x_impulse = 0;
+
+                if (CubeIsSuported() && Keyboard.GetState().IsKeyDown(Keys.Space))
+                {   x_impulse = (int)-(Cube_dimensions[0]/3.3);
+                    jumping_sound.Play();
+                }
+                if (Bg_rect != null)
+                for (int i= 0;i < Bg_rect.Count;i ++)
+                {
+                    Bg_rect[i].x_pos -= speed;
+                    if (Bg_rect[i].x_pos<-70)
+                        Bg_rect.RemoveAt(i);
+
+                }
+                next_obstacle-= speed;
+                if (next_obstacle <= 0)
+                {
+                    Random random = new Random();
+                    int randm = random.Next(1,30);
+                    if (randm >= 30-speed)
+                    {
+                        Bg_rect.Add(new BadGuyRectangle(_graphics.PreferredBackBufferWidth,_graphics.PreferredBackBufferHeight-_graphics.PreferredBackBufferHeight/4,rectangle));
+                        int randm_gap = random.Next(1,Rectangles_dimensions[1]/2);
+                        next_obstacle = 3*Rectangles_dimensions[1] + randm_gap*speed;
+                        
+                    }}}}
+        else
+            if (Keyboard.GetState().IsKeyDown(Keys.Escape))
+                    Exit();
+            if (Keyboard.GetState().IsKeyDown(Keys.Enter))
+            {   
+                if (score>highscore)
+                    highscore = score;
+                score = 0;
+                next_difficulty = 0;
+                
+                died = false;
+
+                Cube_position =  new int[2];
+                Cube_position[0]= 30;
+                Cube_position[1]= _graphics.PreferredBackBufferHeight-_graphics.PreferredBackBufferHeight/4 - _graphics.PreferredBackBufferHeight/10;
+
+                Cube_dimensions = new int[2];
+                Cube_dimensions[0]= _graphics.PreferredBackBufferHeight/10;
+                Cube_dimensions[1]= _graphics.PreferredBackBufferHeight/10;
+
+                Rectangles_dimensions = new int[2];
+                Rectangles_dimensions[0]= _graphics.PreferredBackBufferWidth/16;
+                Rectangles_dimensions[1]= _graphics.PreferredBackBufferHeight/6;
+                x_impulse = 0;
+                speed = Cube_dimensions[0]/10;
+                next_obstacle = 0;
+                Bg_rect = [];
+
+
+            }
 
         base.Update(gameTime);
     }
@@ -136,6 +187,10 @@ public class JumpingCube : Game
     {
         GraphicsDevice.Clear(Color.CornflowerBlue);
         _spriteBatch.Begin();
+        _spriteBatch.DrawString(scores_font, "Score:"+score, new Vector2(10, 10), Color.Black);
+        _spriteBatch.DrawString(scores_font, "Highscore:"+highscore, new Vector2(120, 10), Color.Black);
+        _spriteBatch.DrawString(scores_font, "Difficulty:"+speed/7+"."+speed%7, new Vector2(280, 10), Color.Black);
+
         // Drawn rectangle
         _spriteBatch.Draw(cube ,new Rectangle(Cube_position[0],Cube_position[1],Cube_dimensions[0],Cube_dimensions[1]), Color.OldLace);
         
@@ -144,6 +199,9 @@ public class JumpingCube : Game
         
         // Drawn soil
         DrawLine(new Vector2(0, _graphics.PreferredBackBufferHeight-_graphics.PreferredBackBufferHeight/4), new Vector2(_graphics.PreferredBackBufferWidth, _graphics.PreferredBackBufferHeight-_graphics.PreferredBackBufferHeight/4), 5, Color.Brown);
+        if (died)
+        _spriteBatch.DrawString(scores_font, "You died. To play again press ENTER, to quit press ESC", new Vector2(_graphics.PreferredBackBufferWidth/2-300, _graphics.PreferredBackBufferHeight/2), Color.Black);
+
         _spriteBatch.End();
         
 
